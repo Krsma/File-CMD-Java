36ba8a9ef2f59e72e3c8f1fd4fb0432f551f1dde
Krsma <vlada.krsmanovic@gmail.com>
2018-06-14 10:47:40 +0200

    added copy and exit

--- file_manager.java
+++ file_manager.java
@@ -1,11 +1,13 @@
 import java.io.File;
 import java.util.*;
 
+import com.sun.xml.internal.fastinfoset.stax.factory.StAXOutputFactory;
+
 
 public  class	file_manager
 {
 
-	public void list_dir()
+	public static void list_dir()
 	{
 		try
 		 {
@@ -26,7 +28,7 @@ public  class	file_manager
 
 	}
 	
-	public File file_cd(String argument)
+	public static File file_cd(String argument)
 	{
 
 
@@ -52,8 +54,11 @@ public  class	file_manager
 			File[] list = f.listFiles();
 			for (int i = 0; i < list.length; i++)
 			{
-				if (list[i].getName().equals(argument));
-				return list[i];
+				if (list[i].getName().equals(argument))
+				{
+					return list[i];
+				}
+				
 			}
 			return f;
 
@@ -61,9 +66,52 @@ public  class	file_manager
 
 	}
 
-	public 
+	private static void file_copy(String file_name1, String file_name2 ) throws IOException 
+	{
+		File source = new File(file_name1);
+		File dest = new File(file_name2);
 
-public static void main(String[] args) {
-	
+    InputStream is = null;
+    OutputStream os = null;
+        is = new FileInputStream(source);
+        os = new FileOutputStream(dest);
+        byte[] buffer = new byte[1024];
+        int length;
+				while ((length = is.read(buffer)) > 0) 
+				{
+            os.write(buffer, 0, length);
+        }
+     
+        is.close();
+        os.close();
+    
 }
+
+public static void main(String[] args)
+  {
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+
+		String[] parts = line.split(" ");
+		if (parts[0].equals("exit"))
+		{
+			System.exit(0);
+		}
+		else if (parts[0].equals("cd"))
+		{
+			File f = file_cd(parts[1]);
+			System.out.println(f.getName().toString());
+		}
+		else if (parts[0].equals("dir"))
+		{
+			list_dir();
+		}
+		else if (parts[0].equals("copy"))
+		{
+			file_copy(parts[1], parts[2]);
+		}
+
+ 
+	
+	}
 }
